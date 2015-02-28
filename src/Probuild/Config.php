10c3db05a031afd48bee6931614c838e11b4c23d
Cristian Quiroz <cdquirozc@gmail.com>
2015-02-28 13:08:00 +0000

    Added methods to Config class to read configurations from yaml file.

--- src/Probuild/Config.php
+++ src/Probuild/Config.php
@@ -15,6 +15,18 @@ class Config
     const POST_COMPOSER_DIR_PATHS = 'post-composer-dir-paths';
     const GRUNT = 'grunt';
 
+    /** @var array */
+    protected $data;
+
+    /**
+     * @param $file
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    public function __construct($file)
+    {
+        $this->load($file);
+    }
+
     /**
      * @param string $file
      * @return array
@@ -23,14 +35,87 @@ class Config
      */
     public function load($file)
     {
-        $configData = $this->read($file);
-        $this->validate($configData);
+        $data = $this->read($file);
+        $this->validate($data);
 
-        return $configData;
+        $this->data = $data;
     }
 
     /**
-     * @param $file
+     * @return string
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    public function getTargetDirectory()
+    {
+        return $this->data[self::TARGET_DIR];
+    }
+
+    /**
+     * @return bool
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    public function shouldCleanTargetDirectory()
+    {
+        return array_key_exists(self::CLEAN, $this->data) && $this->data[self::CLEAN] == true;
+    }
+
+    /**
+     * @return array
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    public function getCleanExceptions()
+    {
+        if (!array_key_exists(self::CLEAN_EXCEPTIONS, $this->data) || !is_array($this->data[self::CLEAN_EXCEPTIONS])) {
+            return array();
+        }
+
+        return $this->data[self::CLEAN_EXCEPTIONS];
+    }
+
+    /**
+     * @return array
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    public function getDirectoryPaths()
+    {
+        return $this->data[self::DIR_PATHS];
+    }
+
+    /**
+     * @return bool
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    public function shouldRunComposer()
+    {
+        return array_key_exists(self::COMPOSER, $this->data) && $this->data[self::COMPOSER] == true;
+    }
+
+    /**
+     * @return array
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    public function getPostComposerDirectoryPaths()
+    {
+        if (!array_key_exists(self::POST_COMPOSER_DIR_PATHS, $this->data)
+            || !is_array($this->data[self::POST_COMPOSER_DIR_PATHS])
+        ) {
+            return array();
+        }
+
+        return $this->data[self::POST_COMPOSER_DIR_PATHS];
+    }
+
+    /**
+     * @return bool
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    public function shouldRunGrunt()
+    {
+        return array_key_exists(self::GRUNT, $this->data) && $this->data[self::GRUNT] == true;
+    }
+
+    /**
+     * @param string $file
      * @return array
      * @throws Exception\FileNotExists
      * @author Cristian Quiroz <cris@qcas.co>
@@ -45,7 +130,7 @@ class Config
     }
 
     /**
-     * @param $config
+     * @param array $config
      * @throws Exception\InvalidConfig
      * @author Cristian Quiroz <cris@qcas.co>
      */
@@ -55,7 +140,7 @@ class Config
             throw new Exception\InvalidConfig(sprintf('\'%s\' has not been specified in the config.', self::TARGET_DIR));
         }
 
-        if (!array_key_exists(self::DIR_PATHS, $config)) {
+        if (!array_key_exists(self::DIR_PATHS, $config) || count($config[self::DIR_PATHS]) == 0) {
             throw new Exception\InvalidConfig(sprintf('\'%s\' has not been specified in the config.', self::DIR_PATHS));
         }
     }
