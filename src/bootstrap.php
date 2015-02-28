8b09390e507c376cc470ec6dc6282f6fc4cb4097
Cristian Quiroz <cdquirozc@gmail.com>
2015-02-28 13:26:08 +0000

    Added bootstrap for configuring commands + modified bin/probuild to use it

---
+++ src/bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Probuild\Command;
+use Probuild\Manager;
+use Symfony\Component\Console\Application;
+
+
+$application = new Application('Probuild', '@package_version@');
+
+//Create Shell
+$shell = new \Probuild\Shell();
+
+//Create Managers (they should all share the same Shell object)
+$directoryManager = new Manager\Directory();
+$linkManager = new Manager\Link();
+$composerManager = new Manager\Composer();
+$gruntManager = new Manager\Grunt();
+$directoryManager->setShell($shell);
+$linkManager->setShell($shell);
+$composerManager->setShell($shell);
+$gruntManager->setShell($shell);
+
+//Create and configure Make Command
+$makeCommand = new Command\MakeCommand();
+$makeCommand
+    ->setShell($shell)
+    ->setDirectoryManager($directoryManager)
+    ->setLinkManager($linkManager)
+    ->setComposerManager($composerManager)
+    ->setGruntManager($gruntManager);
+
+$application->add($makeCommand);
+
+return $application;
