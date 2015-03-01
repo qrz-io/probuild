dd99e7de79b1aef4f1216dca0273ca2369aee333
Cristian Quiroz <cdquirozc@gmail.com>
2015-03-01 15:05:59 +0000

    Adjustments to shell output

--- src/Probuild/Manager/Composer.php
+++ src/Probuild/Manager/Composer.php
@@ -3,17 +3,25 @@
 namespace Probuild\Manager;
 
 use Probuild\AbstractManager;
-use Probuild\Shell;
 
 class Composer extends AbstractManager
 {
 
     /**
      * @param string $targetDir
+     * @return Composer
      * @author Cristian Quiroz <cris@qcas.co>
      */
     public function run($targetDir)
     {
+        if (!`which composer`) {
+            $this->write('<error>\'composer\' is not installed.</error>');
+            return $this;
+        }
+        $this->write(
+            $this->getShell()->exec("composer update -d {$targetDir}")
+        );
 
+        return $this;
     }
 }
