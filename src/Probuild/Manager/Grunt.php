94859dd99b683cbf659cba9ccdb428a7d41f1202
Cristian Quiroz <cdquirozc@gmail.com>
2015-03-01 15:06:28 +0000

    Implemented grunt manager

--- src/Probuild/Manager/Grunt.php
+++ src/Probuild/Manager/Grunt.php
@@ -3,17 +3,42 @@
 namespace Probuild\Manager;
 
 use Probuild\AbstractManager;
-use Probuild\Shell;
 
 class Grunt extends AbstractManager
 {
 
     /**
      * @param string $targetDir
+     * @return Grunt
      * @author Cristian Quiroz <cris@qcas.co>
      */
     public function run($targetDir)
     {
+        if (!`which npm`) {
+            $this->write('<error>\'npm\' is not installed. </error>');
+            return $this;
+        }
 
+        if (!`which grunt`) {
+            $this->write('<error>\'grunt\' is not installed. </error>');
+            return $this;
+        }
+
+        $gruntfilePath = $targetDir . '/Gruntfile.js';
+        if (!is_file($gruntfilePath)) {
+            $this->write(
+                sprintf('<error>Cannot find gruntfile in \'%s\'</error>', $gruntfilePath)
+            );
+            return $this;
+        }
+
+        $this->write(
+            $this->getShell()->exec("npm install --prefix {$targetDir} {$targetDir}")
+        );
+        $this->write(
+            $this->getShell()->exec("grunt --gruntfile {$targetDir}/Gruntfile.js devbuild-force")
+        );
+
+        return $this;
     }
 }
